e36c01a80edfc9e3c02cd747ad851531ab207278
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-07-13 18:07:44 +0600

    reduce quantity after order placed

--- src/modules/orderInfo/orderInfo.service.ts
+++ src/modules/orderInfo/orderInfo.service.ts
@@ -1,14 +1,54 @@
+import mongoose from "mongoose";
 import { TOrderInfo } from "./orderInfo.interface"
 import { OrderInfo } from "./orderInfo.model"
+import { AddProduct } from "../addProducts/addProducts.model";
+
+
 
 
 const createOrderInfo = async(payload:TOrderInfo) =>{
-    // console.log(payload)
+    console.log(payload)
+    
+    
 
+    // const productIds = [];
+
+for (const product of payload.products) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const productId = product.productId
+        const number = product.number
+        console.log(number)
+        const existingProduct = await AddProduct.findById(productId).session(session)
+        console.log(existingProduct?.quantity)
+        existingProduct?.quantity -=  number
+
+
+        await existingProduct?.save({ session });
+
+        const orderInfo = new OrderInfo(payload);
+        await orderInfo.save({ session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return orderInfo
+        
+
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        console.log(error)
+        
+    }
+  
+}
     
     
-    const result =await OrderInfo.create(payload)
-    return result
+    // const result =await OrderInfo.create(payload)
+    return {}
 
 }
 
